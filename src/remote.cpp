48b4b53cc1ea494b2d29ae89020dbbe247bbcdd4
Farfan <ifarfane0900@ipn.mx>
2011-11-21 21:10:55 -0800

    Prototype for retrieving hosts list partially working

--- src/remote.cpp
+++ src/remote.cpp
@@ -23,6 +23,8 @@ Thiao.  If not, see <http://www.gnu.org/licenses/>.
 
  */
 
+#include <xmlrpc-c/client_simple.hpp>
+#include <xmlrpc-c/base.hpp>
 #include "thiao.h"
 #include "remote.h"
 
@@ -31,18 +33,23 @@ Thiao.  If not, see <http://www.gnu.org/licenses/>.
 /*
  * Constructor with parameters
  */
-HostLoad::HostLoad(string h, float l){
+Host::Host(string h, float l){
     this->host = h;
     this->load = l;
 }
 
+/*
+ * Only the name of the host
+ */
+Host::Host(string h){
+    Host(h, 0);
+}
 
 /*
  * Default constructor
  */
-HostLoad::HostLoad(){
-    this->host = "";
-    this->load = 0;
+Host::Host(){
+    Host("", 0);
 }
 
 
@@ -53,9 +60,10 @@ HostLoad::HostLoad(){
  * @param h2: host 2
  * @return: true if the load of host 1 is lower than the load of host 2
  */
-bool compare_host_load(class HostLoad h1, class HostLoad h2){
+bool compare_host_load(class Host h1, class Host h2){
     return h1.load < h2.load;
 }
+
 
 
 /*
@@ -64,9 +72,9 @@ bool compare_host_load(class HostLoad h1, class HostLoad h2){
  * @return: Host + load ordered by CPU loads
  * //TODO: declare hosts as const?
  */
-list<class HostLoad> get_host_load(list<string> hosts){
-    list<class HostLoad> hl;
-    HostLoad load;
+list<class Host> get_host_load(list<string> hosts){
+    list<class Host> hl;
+    Host load;
     float min1, min5, min15;
 
     while( !hosts.empty() ){
@@ -74,13 +82,56 @@ list<class HostLoad> get_host_load(list<string> hosts){
         cout << cmd << endl;
         FILE *f = popen(cmd.c_str(), "r");
         fscanf(f, "%f %f %f", &min1, &min5, &min15);
-        load = HostLoad(hosts.front(), min1);
+        load = Host(hosts.front(), min1);
         hl.push_back(load);
         hosts.pop_front();
         fclose(f);
     }
     hl.sort(compare_host_load);
     return hl;
+}
+
+
+
+void fill_host_list(list<Host> hosts){
+    xmlrpc_c::clientSimple client;
+    xmlrpc_c::value result_rpc, first, second;
+    string const serverUrl = "http://localhost:2633/RPC2";
+    string const service = "one.hostpool.info";
+
+//TODO: put this in a try/catch block
+    cout << "calling hostpool" << endl;
+    client.call(serverUrl, service, "s", &result_rpc, "");
+
+    if ( result_rpc.type() != 6 ){ // array
+        cout << "error?" << endl;
+        return;
+    }
+    cout << "converting to array" << endl;
+    xmlrpc_c::value_array result_array(result_rpc);
+    vector<xmlrpc_c::value> const result_vector(result_array.vectorValueValue());
+
+    cout << "vector size: " << result_vector.size() << endl;
+    // the first result tell whether it was successful or not
+    first = static_cast<xmlrpc_c::value>(result_vector[0]);
+    xmlrpc_c::value_boolean status = static_cast<xmlrpc_c::value>(first);
+
+    // the second is a string with something hopefully useful
+    second = static_cast<xmlrpc_c::value>(result_vector[1]);
+    xmlrpc_c::value_string msg = static_cast<xmlrpc_c::value_string>(second);
+
+
+    //---------- try to do something useful with the result ---------------
+
+    if ( static_cast<bool>(status) ){
+        cout << "siii" << endl;
+        cout << static_cast<string>(msg) << endl;
+    } else {
+        cout << "noo" << endl;
+        cout << static_cast<string>(msg) << endl;
+    }
+
+
 }
 
 
