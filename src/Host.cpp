54859612bace95a54b6265783aca3ed402245c42
Farfan <ifarfane0900@ipn.mx>
2011-11-22 17:10:25 -0800

    Retrieves the avg cpu load of the host

--- src/Host.cpp
+++ src/Host.cpp
@@ -7,32 +7,27 @@
 
 #include "Host.h"
 
-/*
- * Constructor with parameters
- */
+
 Host::Host(int id, string name, int state){
     this->name = name;
     this->id = id;
     this->state = state;
 }
 
-/*
- * Only the name of the host
- */
-Host::Host(string h){
+
+
+Host::Host(string name){
 //    Host(h, 0, 0);
 }
 
-/*
- * Default constructor
- */
+
+
 Host::Host(){
     Host("");
 }
 
-/*
- * Creates the host out of a xmlrpc host definition node
- */
+
+
 Host::Host(TiXmlNode *host_node){
     TiXmlNode *child;
 
@@ -63,4 +58,27 @@ void Host::get_load(void){
     //TODO
 }
 
+
+
+float Host::retrieve_host_load(void){
+    string cmd = "ssh oneadmin@" + this->name + " cat /proc/loadavg";
+//    cout << cmd << endl;
+    FILE *f = popen(cmd.c_str(), "r");
+    fscanf(f, "%f %f %f", &this->load_m1, &this->load_m5, &this->load_m15);
+//        load = Host(hosts.front(), min1);
+    fclose(f);
+    return this->load_m1;
+}
+
+
+
+/*
+ * Compare the load of 2 hosts
+ * @param h1: host 1
+ * @param h2: host 2
+ * @return: true if the load of host 1 is lower than the load of host 2
+ */
+bool compare_host_load(class Host h1, class Host h2){
+    return h1.load_m1 < h2.load_m1;
+}
 
