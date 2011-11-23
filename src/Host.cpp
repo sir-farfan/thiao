446989357509a6bad01924127b234f82393381db
Farfan <ifarfane0900@ipn.mx>
2011-11-22 16:49:20 -0800

    Creates from an xml node

--- src/Host.cpp
+++ src/Host.cpp
@@ -30,6 +30,33 @@ Host::Host(){
     Host("");
 }
 
+/*
+ * Creates the host out of a xmlrpc host definition node
+ */
+Host::Host(TiXmlNode *host_node){
+    TiXmlNode *child;
+
+//    cout << "type: " << host_node->Type();
+//    cout << " value: " << host_node->ValueStr() << endl;
+    if ( host_node->ValueStr() != "HOST" ){
+        cout << "Error, this is not a HOST node";
+        Host();
+        return;
+    }
+
+    child = host_node->FirstChild("ID");
+//    cout << "ID type: " << child->Type() << endl;
+//    cout << "ID child type: " << child->FirstChild()->Type() << endl;
+//    cout << "id child value: " << child->FirstChild()->Value() << endl;
+    this->id = atoi( child->FirstChild()->Value() );
+
+    child = host_node->FirstChild("NAME");
+    this->name = child->FirstChild()->ValueStr();
+
+    child = host_node->FirstChild("STATE");
+    this->state = atoi( child->FirstChild()->Value() );
+}
+
 
 
 void Host::get_load(void){
